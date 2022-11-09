55fedc6b6439de47c9d7c298a84621193000838a
HP <vtu15892@veltech.edu.in>
2022-11-09 18:52:25 +0530

    form validation added (updated)

--- app/Http/Controllers/loginController.php
+++ app/Http/Controllers/loginController.php
@@ -12,6 +12,11 @@ class loginController extends Controller
     }
     public function loginSubmit(Request $request)
     {
+        $validatedData = $request->validate(
+            ['email'=>'required|email',
+            'password'=>'required|min:6|max:12']
+        );
+
         $email = $request->input('email');
         $password = $request->input('password');
         return 'Email : '.$email . " Password : ".$password;
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -21,11 +21,17 @@
                         @csrf
                         <div class="mb-3">
                           <label for="email" class="form-label">Email address</label>
-                          <input type="email" class="form-control" id="email" aria-describedby="emailHelp" name= "email">
+                          <input type="email" class="form-control" id="email" aria-describedby="emailHelp" name= "email" value={{old('email')}}>
+                          @error('email')
+                          <span class='text-danger'>{{$message}}</span>
+                          @enderror   
                         </div>
                         <div class="mb-3">
                           <label for="password" class="form-label">Password</label>
-                          <input type="password" class="form-control" id="password" name="password">
+                          <input type="password" class="form-control" id="password" name="password" value={{old('password')}}>
+                          @error('password')
+                          <span class='text-danger'>{{$message}}</span>
+                          @enderror
                         </div>
                         <button type="submit" class="btn btn-primary">Submit</button>
                       </form>
