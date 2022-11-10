841525248a3000330be604263f12ad0bc6503490
HP <vtu15892@veltech.edu.in>
2022-11-10 19:39:53 +0530

    pagination added

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,7 +4,8 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use Illuminate\Support\Facades\DB;
+use Faker\Factory as Faker;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -20,8 +21,17 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
-        $this->call([
-                PostTableSeeder::class,
+        // $this->call([
+        //         PostTableSeeder::class,
+        //     ]);
+        $faker = Faker::create();
+        foreach(range(20,1000) as $index)
+        {
+            DB::table('users')->insert([
+                'user_name'=>$faker->name,
+                'user_phone'=>$faker->phoneNumber,
+                'user_email'=>$faker->email,
             ]);
+        }
     }
 }
